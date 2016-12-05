bf9c1dd3849f21111001e955ef23b3483e0a537f
Ales Kafka <ales.kafka@gmail.com>
2016-12-05 14:37:02 +0100

    Refactor api/channel addListener

--- src/api/channel.js
+++ src/api/channel.js
@@ -3,35 +3,54 @@ const listeners = [];
 
 export default {
 	listen(state=[], type, action, fn) {
-		if (typeof action==='function') {
-			fn = action;
-			action = null;
-		}
-
-		const listener = { type, action, fn };
-		state.push(listener);
-		listeners.push(listener);
+		state.push(createListener(type, action, fn));
 		return state;
 	},
 	dispatch(state, type, action, ...args) {
 		dispatch(type, action, ...args);
 	},
 	destroy(state) {
-		state.
-			filter(listener => listeners.indexOf(listener)!==-1).
-			forEach(listener => listeners.splice(listeners.indexOf(listener), 1));
+		state.forEach(listener => listener());
 	},
 }
 
 
 export function addListener(type, action, fn) {
-	const listener = { type, action, fn };
-	listeners.push(listener);
+	return createListener(type, action, fn);
 }
 
 
 export function dispatch(type, action, ...args) {
 	listeners.
-		filter(listener => listener.type===type && (listener.action===null || listener.action===action)).
-		forEach(({ fn }) => fn(...args));
+		filter(listener => listener.type===null || listener.type===type).
+		filter(listener =>  listener.action===null || listener.action===action).
+		forEach(({ fn }) => fn(...arguments));
+}
+
+
+function createListener(type, action, fn) {
+	if (typeof type==='function') {
+		const _fn = type;
+		action = null;
+		type = null;
+		fn = function(...args) { _fn(...args); };
+
+	} else if (typeof action==='function') {
+		const _fn = action;
+		action = null;
+		fn = function(type, ...args) { _fn(...args); };
+
+	} else {
+		const _fn = fn;
+		fn = function(type, action, ...args) { _fn(...args); };
+	}
+
+	const listener = { type, action, fn };
+	listeners.push(listener);
+
+	return function() {
+		if (listeners.indexOf(listener)!==-1) {
+			listeners.splice(listeners.indexOf(listener), 1)
+		}
+	}
 }
