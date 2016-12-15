e90b03cbd1414a14528796fddabb6ea92feca498
Ales Kafka <ales.kafka@gmail.com>
2016-12-15 16:37:46 +0100

    Export selection functions in closure

--- closure/src/selection.js
+++ closure/src/selection.js
@@ -7,3 +7,8 @@ goog.require('goog.dom.selection');
 exports.setCursorAtEnd = function($element) {
 	goog.dom.selection.setCursorPosition($element, $element.value.length);
 }
+
+exports.getStart = goog.dom.selection.getStart;
+exports.getEnd = goog.dom.selection.getEnd;
+exports.setStart = goog.dom.selection.setStart;
+exports.setEnd = goog.dom.selection.setEnd;
