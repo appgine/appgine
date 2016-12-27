ab1282f92942a2dabf5eddfb6a80ee2a102ecf8d
Ales Kafka <ales.kafka@gmail.com>
2016-12-27 21:42:53 +0100

    Add Dynamic context support in AppComponent

--- src/helpers/AppComponent.js
+++ src/helpers/AppComponent.js
@@ -18,13 +18,70 @@ export class AppComponent extends Component {
 
 	static childContextTypes = contextTypes;
 
+	constructor(props) {
+		super(props);
+		this.DynamicAppProps = {};
+		this.componentWillReceiveProps(props);
+	}
+
 	getChildContext() {
 		return context;
 	}
 
-	render() {
-		const { children } = this.props;
-		return children;
+	componentWillReceiveProps(nextProps) {
+		let shouldUpdate = false;
+		const { children, ...DynamicAppProps } = nextProps;
+
+		if (Object.keys(this.DynamicAppProps).length!==Object.keys(DynamicAppProps).length) {
+			shouldUpdate = true;
+		}
+
+		Object.keys(DynamicAppProps).
+			filter(key => DynamicAppProps[key]!==this.DynamicAppProps[key]).
+			forEach(key => shouldUpdate = true);
+
+		if (shouldUpdate) {
+			this.DynamicAppProps = DynamicAppProps;
+			this.DynamicAppComponent = DynamicAppComponent(DynamicAppProps);
+		}
 	}
 
+	render() {
+		const DynamicAppComponent = this.DynamicAppComponent;
+		const { children } = this.props;
+
+		if (DynamicAppComponent) {
+			return <DynamicAppComponent>{children}</DynamicAppComponent>
+
+		} else {
+			return children;
+		}
+	}
+
+}
+
+
+function DynamicAppComponent(context) {
+	const contextTypes = {};
+
+	Object.keys(context).forEach(function(key) {
+		if (typeof context[key]==='function') {
+			contextTypes[key] = React.PropTypes.func;
+
+		} else {
+			contextTypes[key] = React.PropTypes.object;
+		}
+	});
+
+	return class DynamicAppComponent_ extends Component {
+		static childContextTypes = contextTypes;
+
+		getChildContext() {
+			return context;
+		}
+
+		render() {
+			return this.props.children;
+		}
+	}
 }
