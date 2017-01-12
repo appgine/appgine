2a6669c7b6f5d09a1f684d814dbf4b99e4959798
Ales Kafka <ales.kafka@gmail.com>
2017-01-12 14:31:01 +0100

    Add default createTargets fn

--- src/api/targets/index.js
+++ src/api/targets/index.js
@@ -4,7 +4,7 @@ import TargetList from './TargetList'
 
 export default {
 	createTargets(state, ...args) {
-		const fn = args.pop();
+		const fn = args.pop() || function() {};
 		state = new TargetList(...args);
 		fn(state);
 		return state;
