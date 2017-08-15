e1fe96c56c847d5fc2f4da8fd9a039dbe392818a
Ales Kafka <ales.kafka@gmail.com>
2017-08-15 11:52:30 +0200

    Fix api/update to remove from state and call destroy

--- src/api/update.js
+++ src/api/update.js
@@ -1,15 +1,22 @@
 
+import { destroy } from 'plugin-macro-loader/lib/lib/destroy'
+
 
 export default {
 	initialState: [],
 	update(state, methods) {
 		state.push(methods);
+		return function() {
+			if (state.indexOf(methods)!==-1) {
+				state.splice(state.indexOf(methods), 1);
+			}
+
+			destroy(methods);
+		}
 	},
 	destroy(state) {
-		for (let _state of state) {
-			if (_state.destroy) {
-				_state.destroy();
-			}
+		for (let methods of state) {
+			destroy(methods);
 		}
 	},
 }
