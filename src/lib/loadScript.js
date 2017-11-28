c3ca8d2b655d3f6554a8ec144deb605ad25f721e
Ales Kafka <ales.kafka@gmail.com>
2017-11-28 11:54:56 +0100

    Add module loadScript

---
+++ src/lib/loadScript.js
@@ -0,0 +1,36 @@
+
+
+const loading = {};
+
+export default function loadScript(src, onLoad) {
+	if (loading[src]===true) {
+		return onLoad(false);
+
+	} else if (loading[src]===false) {
+		return null;
+
+	} else if (loading[src]===undefined) {
+		loading[src] = [onLoad];
+
+		const $script = document.createElement('script');
+		$script.async = 1;
+		$script.onload = function() {
+			const callbacks = loading[src];
+			const first = callbacks.shift();
+
+			loading[src] = true;
+			first && first(true);
+			callbacks.forEach(fn => fn && fn(false));
+		}
+		$script.onerror = function() {
+			loading[src] = false;
+		}
+		$script.src = src;
+
+		while (!(document.querySelector('head') || document.documentElement).appendChild);
+		(document.querySelector('head') || document.documentElement).appendChild($script);
+
+	} else {
+		loading[src].push(onLoad);
+	}
+}
