70f7475f2e3188b2bdc31455ee36578b3fe791bc
Ales Kafka <ales.kafka@gmail.com>
2017-12-04 14:12:45 +0100

    Add Element.prototype.matches polyfill

--- src/shim.js
+++ src/shim.js
@@ -45,3 +45,21 @@ if (document.contains===undefined) {
             clearTimeout(id);
         };
 }());
+
+
+(function() {
+  if (!Element.prototype.matches) {
+      Element.prototype.matches =
+          Element.prototype.matchesSelector ||
+          Element.prototype.mozMatchesSelector ||
+          Element.prototype.msMatchesSelector ||
+          Element.prototype.oMatchesSelector ||
+          Element.prototype.webkitMatchesSelector ||
+          function(s) {
+              var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+                  i = matches.length;
+              while (--i >= 0 && matches.item(i) !== this) {}
+              return i > -1;
+          };
+  }
+})();
