43f6b9c45dde3f2627d729cbfdf8f0b797923fa0
Ales Kafka <ales.kafka@gmail.com>
2018-03-26 09:52:41 +0200

    Add export of appgine.string.toAscii

--- closure/src/string.js
+++ closure/src/string.js
@@ -2,7 +2,9 @@
 goog.module('string');
 
 goog.require('goog.string');
+goog.require('appgine.string');
 
 
 exports.collapseWhitespace = goog.string.collapseWhitespace;
 exports.getRandomString = goog.string.getRandomString;
+exports.toAscii = appgine.string.toAscii;
