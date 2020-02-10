d8bd589bb53b84c40c4efbc712ec54a9a0ef4150
Ales Kafka <aleskafka@Ales-MacBook-Pro-2.local>
2020-02-10 11:45:04 +0100

    Refactor api/swap to add swapSelector function

--- src/api/swap.js
+++ src/api/swap.js
@@ -55,22 +55,7 @@ export function createSwapping(request, isCurrent) {
 			if (isCurrent) {
 				swapDocument(function() {
 					Object.keys(pending).forEach(function(swapId) {
-						const $staticSwap = document.querySelector('[data-static] [data-swap="'+swapId+'"]');
-						const $atomicSwap = document.querySelector('[data-atomic] [data-swap="'+swapId+'"]');
-						const $documentSwap = document.querySelector('[data-swap="'+swapId+'"]');
-
-						if ($staticSwap) {
-							swapElement($staticSwap, pending[swapId], loadStatic, unloadStatic);
-
-						} else if ($atomicSwap) {
-							swapElement($atomicSwap, pending[swapId], loadAtomic, unloadAtomic, request);
-
-						} else if ($documentSwap) {
-							swapElement($documentSwap, pending[swapId], load, unload);
-
-						} else if (swapId==='title' || swapId==='document-title') {
-							document.title = pending[swapId];
-						}
+						swapSelectorInner('[data-swap="'+swapId+'"]', pending[swapId], swapId==='title' || swapId==='document-title', request);
 					});
 				});
 			}
@@ -80,6 +65,35 @@ export function createSwapping(request, isCurrent) {
 	return swapping;
 }
 
+
+export function swapSelector(...args) {
+	swapDocument(function() {
+		swapSelectorInner(...args);
+	});
+}
+
+
+function swapSelectorInner(selector, content, isTitle, request, fn) {
+	const $staticSwap = document.querySelector('[data-static] ' + selector);
+	const $atomicSwap = document.querySelector('[data-atomic] ' + selector);
+	const $documentSwap = document.querySelector(selector);
+
+	if ($staticSwap) {
+		swapElement($staticSwap, content, loadStatic, unloadStatic);
+		fn && fn($staticSwap);
+
+	} else if ($atomicSwap) {
+		swapElement($atomicSwap, content, loadAtomic, unloadAtomic, request);
+		fn && fn($atomicSwap);
+
+	} else if ($documentSwap) {
+		swapElement($documentSwap, content, load, unload);
+		fn && fn($documentSwap);
+
+	} else if (isTitle) {
+		document.title = content;
+	}
+}
 
 
 function swapElement($swap, content, load, unload, request) {
