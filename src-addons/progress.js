98e6742d001b058fcdefa247af6e8a36e59be547
Ales Kafka <aleskafka@Ales-MacBook-Pro-2.local>
2020-02-29 15:08:13 +0100

    Add addons/progress touch support

--- src-addons/progress.js
+++ src-addons/progress.js
@@ -1,69 +1,105 @@
 
 
-export default function createProgress(e, onValue, events={})
+export default function createProgress($element, onValue, events={})
 {
 	if (!(onValue instanceof Function)) {
 		events = onValue;
 		onValue = percent => percent;
 	}
 
-	if (e.which===1 && e.currentTarget) {
-		e.preventDefault();
+	const eventStart = 'ontouchstart' in document.documentElement ? 'touchstart' : 'mousedown';
+	const eventMove = 'ontouchmove' in document.documentElement ? 'touchmove' : 'mousemove';
+	const eventEnd = 'ontouchstart' in document.documentElement ? 'touchend' : 'mouseup';
 
-		const { left, width } = e.currentTarget.getBoundingClientRect();
+	let progress;
+	function onEventStart(e) {
+		progress && progress();
 
-		let value = onValue(Math.max(0, Math.min(width, e.screenX-left))/width);
-		events.onStart && events.onStart(value);
+		if ((e.which!==2 && e.which!==3) && e.currentTarget) {
+			e.preventDefault();
+
+			const { left, width } = e.currentTarget.getBoundingClientRect();
+			function onEventValue(event) {
+				const x = event.targetTouches && (event.targetTouches[0] || event.changedTouches[0]).clientX || event.screenX || 0;
+				const value1 = Math.max(0, Math.min(width, x-left))/width;
+				const value2 = onValue && onValue(value1)
+				return value2===undefined ? value1 : value2;
+			}
+
+			function onEventDestroy(e) {
+				progress = null;
+			}
+
+			progress = createProgressEvent(e, eventMove, eventEnd, events, onEventValue, onEventDestroy);
+		}
+	}
+
+	$element.addEventListener(eventStart, onEventStart, {passive: false});
+
+	function destroy() {
+		$element.removeEventListener(eventStart, onEventStart, {passive: false})
+		progress && progress();
+		progress = null;
+	}
+
+	destroy.isRunning = function() { return !!progress; }
+	return destroy;
+}
+
+
+function createProgressEvent(event, eventMove, eventEnd, events, onValue, onDestroy) {
+	let value = onValue(event);
+
+	if (events.onStart && events.onStart(value) !== undefined) {
+		return null;
+	}
+
+	events.onProgress && events.onProgress(value);
+
+	function onMove(e) {
+		value = onValue(e);
 		events.onProgress && events.onProgress(value);
+	}
 
-		function onMouseMove(e) {
-			if (e.which) {
-				value = onValue(Math.max(0, Math.min(width, e.screenX-left))/width);
-				events.onProgress && events.onProgress(value);
+	function onDragStart(e) {
+		if (end()) {
+			events.onEnd && events.onEnd();
+		}
+	}
 
-			} else if (end()) {
-				events.onDone && events.onDone(value);
+	function onEnd(e) {
+		if (event.target===e.target && e.timeStamp-event.timeStamp<300) {
+			if (events.onClick && events.onClick(e) !== undefined) {
+				end();
 			}
 		}
 
-		function onDragStart(e) {
-			if (end()) {
-				events.onEnd && events.onEnd();
-			}
+		if (end()) {
+			value = onValue(e);
+			events.onProgress && events.onProgress(value);
+			events.onDone && events.onDone(value);
+		}
+	}
+
+	let ended = false;
+	function end() {
+		if (ended===false) {
+			ended = true;
+			document.removeEventListener(eventEnd, onEnd);
+			document.removeEventListener(eventMove, onMove);
+			document.removeEventListener('dragstart', onDragStart);
+			onDestroy();
+			return true;
 		}
 
-		function onMouseUp(e) {
-			if (end()) {
-				value = onValue(Math.max(0, Math.min(width, e.screenX-left))/width);
-				events.onProgress && events.onProgress(value);
-				events.onDone && events.onDone(value);
-			}
-		}
+		return false;
+	}
 
-		let ended = false;
-		function end() {
-			if (ended===false) {
-				ended = true;
-				document.removeEventListener('mousemove', onMouseMove);
-				document.removeEventListener('mouseup', onMouseUp);
-				document.removeEventListener('dragstart', onDragStart);
-				return true;
-			}
+	document.addEventListener('dragstart', onDragStart);
+	document.addEventListener(eventMove, onMove);
+	document.addEventListener(eventEnd, onEnd);
 
-			return false;
-		}
-
-		document.addEventListener('mousemove', onMouseMove);
-		document.addEventListener('dragstart', onDragStart);
-		document.addEventListener('mouseup', onMouseUp);
-
-		return {
-			abort() {
-				if (end()) {
-					value = onValue(Math.max(0, Math.min(width, e.screenX-left))/width);
-					events.onAbort && events.onAbort(value);
-				}
-			}
-		}
+	return function() {
+		end() && events.onAbort && events.onAbort();
 	}
 }
