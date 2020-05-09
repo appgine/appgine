bef6afad4c6abbd9258ff719ca29fa4cf951be46
Ales Kafka <ales.kafka@gmail.com>
2020-05-09 20:01:53 +0200

    Remove unused helpers

--- src/helpers.js
+++
@@ -1,8 +0,0 @@
-
-export * from './helpers/createConnector'
-export * from './helpers/createAsyncComponent'
-export * from './helpers/createComponent'
-export * from './helpers/pluginObj'
-
-export * from './helpers/AppComponent'
-export * from './helpers/StoreComponent'
--- src/helpers/pluginObj.js
+++
@@ -1,6 +0,0 @@
-
-
-export function pluginObj(plugin) {
-	plugin.argumentObj = true;
-	return plugin;
-}
