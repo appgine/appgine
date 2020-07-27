5e9f93669314f6ad5dd20b18ebcfd31b82c9163f
Ales Kafka <ales.kafka@gmail.com>
2020-07-27 10:20:03 +0200

    Add hooks/timer useInterval and bindInterval support

--- src-hooks/timer.js
+++ src-hooks/timer.js
@@ -1,5 +1,5 @@
 
-import { useContext, bindContextWithDestroy } from 'appgine/hooks'
+import { useContext, bindContextWithDestroy, bindContext } from 'appgine/hooks'
 
 
 export function useTimeout(fn, timeout) {
@@ -12,4 +12,26 @@ export function useTimeout(fn, timeout) {
 
 export function bindTimeout() {
 	return bindContextWithDestroy(useTimeout);
+}
+
+
+export function useInterval(fn, timeout) {
+	return useContext(function() {
+		let pointer = setInterval(fn, timeout)
+		return () => clearInterval(pointer);
+	});
+}
+
+
+export function bindInterval(...args) {
+	let pointer = null;
+	return (args.length<=1 ? bindContextWithDestroy : bindContext)(function(...argsInternal) {
+		return useContext(function() {
+			pointer = pointer || setInterval(...args, ...argsInternal);
+			return function() {
+				clearInterval(pointer);
+				pointer = null;
+			}
+		});
+	});
 }
