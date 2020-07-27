1c6fe3baa2e7ad0a76be8d82ba5e272510710175
Ales Kafka <ales.kafka@gmail.com>
2020-07-27 10:20:32 +0200

    Fix hooks/now missing context

--- src-hooks/now.js
+++ src-hooks/now.js
@@ -1,8 +1,10 @@
 
 import { requestStack } from '../lib/engine/run'
+import { getContext } from 'appgine/hooks'
 
 
 export default function now() {
-	const request = requestStack.findRequest(this.$element);
+	const context = getContext();
+	const request = requestStack.findRequest(context && context.$element);
 	return request && request.created || Date.now();
 }
