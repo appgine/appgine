d7abcce3c2124a4493956e58684b92736ea3f6d9
Ales Kafka <ales.kafka@gmail.com>
2020-08-06 09:17:27 +0200

    Refactor bridges/cssReload into closure/crc32

--- bridges/cssReload.js
+++ bridges/cssReload.js
@@ -1,5 +1,5 @@
 
-import { browser } from 'appgine/closure'
+import { crc32, browser } from 'appgine/closure'
 
 
 function defaultTransform(href) {
@@ -258,31 +258,4 @@ function insertStyleLink($link) {
 	} else {
 		document.head.appendChild($link);
 	}
-}
-
-
-let crcTable = null;
-function createCRCTable() {
-	let c;
-	let crcTable = [];
-	for(let n =0; n < 256; n++){
-		c = n;
-		for(let k =0; k < 8; k++){
-			c = ((c&1) ? (0xEDB88320 ^ (c >>> 1)) : (c >>> 1));
-		}
-		crcTable[n] = c;
-	}
-
-	return crcTable;
-}
-
-function crc32(str) {
-	crcTable = crcTable || createCRCTable();
-
-	let crc = 0 ^ (-1);
-	for (let i = 0; i < str.length; i++ ) {
-		crc = (crc >>> 8) ^ crcTable[(crc ^ str.charCodeAt(i)) & 0xFF];
-	}
-
-	return (crc ^ (-1)) >>> 0;
 }
