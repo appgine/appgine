49aeda4fbfa03571f3130147e261b9d8af27ec79
Ales Kafka <ales.kafka@gmail.com>
2020-10-29 12:22:21 +0100

    Refactor hooks/progress

--- addons/plugins/progress.js
+++ addons/plugins/progress.js
@@ -1,5 +1,5 @@
 
-import { useProgress } from 'appgine/hooks/progress'
+import { useAppProgress } from 'appgine/hooks/progress'
 import { bindTimeout, useTimeout } from 'appgine/hooks/timer'
 
 
@@ -9,12 +9,7 @@ export default function create($root) {
 	$root.classList.add('progress');
 	$root.appendChild($container);
 
-	if ($container.animate) {
-		useProgress(createJSProgress($container));
-
-	} else {
-		useProgress(createCSSProgress($root, $container));
-	}
+	useAppProgress($container.animate ? createJSProgress($container) : createCSSProgress($root, $container));
 }
 
 
@@ -22,42 +17,41 @@ function createCSSProgress($root, $container)
 {
 	const [ usePendingTimeout, destroyPendingTimeout ] = bindTimeout();
 
-	return {
-		start() {
-			destroyPendingTimeout();
+	return function() {
+		destroyPendingTimeout();
+		$root.classList.remove('progress-loading', 'progress-loaded', 'progress-hidden');
 
-			$root.classList.remove('progress-loading');
-			$root.classList.remove('progress-loaded');
-			$root.classList.remove('progress-hidden');
+		useTimeout(() => {
+			$root.classList.add('progress-loading');
+			$container.style.width = '';
+			$container.style.animationDuration = '';
+		}, 0);
 
-			useTimeout(() => {
-				$root.classList.add('progress-loading');
+		return {
+			response() {
+				$container.style.width = $container.getBoundingClientRect().width + 'px';
+				$root.classList.remove('progress-loading');
+			},
+			replace() {},
+			end() {
+				$root.classList.remove('progress-loading');
 				$container.style.width = '';
 				$container.style.animationDuration = '';
-			}, 0);
-		},
-		response() {
-			$container.style.width = $container.getBoundingClientRect().width + 'px';
-			$root.classList.remove('progress-loading');
-		},
-		end() {
-			$root.classList.remove('progress-loading');
-			$container.style.width = '';
-			$container.style.animationDuration = '';
-			$root.classList.add('progress-loaded');
+				$root.classList.add('progress-loaded');
 
-			usePendingTimeout(() => $root.classList.add('progress-hidden'), 1000);
-		},
-		abort() {
-			$root.classList.remove('progress-loading');
-			$container.style.width = '';
-			$container.style.animationDuration = '';
-		},
-		destroy() {
-			$container.style.width = '';
-			$container.style.animationDuration = '';
-			$root.classList.remove('progress-loading', 'progress-loaded', 'progress-hidden');
-		},
+				usePendingTimeout(() => $root.classList.add('progress-hidden'), 1000);
+			},
+			abort() {
+				$root.classList.remove('progress-loading');
+				$container.style.width = '';
+				$container.style.animationDuration = '';
+			},
+			destroy() {
+				$container.style.width = '';
+				$container.style.animationDuration = '';
+				$root.classList.remove('progress-loading', 'progress-loaded', 'progress-hidden');
+			},
+		}
 	}
 }
 
@@ -66,66 +60,45 @@ function createJSProgress($container)
 {
 	let jsAnimation = null;
 
-	return {
-		start() {
-			$container.style.width = '0%';
-			$container.style.visibility = '';
+	return function() {
+		$container.style.width = '0%';
+		$container.style.visibility = '';
 
-			useTimeout(() => {
-				jsAnimation && jsAnimation.cancel();
-				jsAnimation = $container.animate([
-					{width: '2%', offset: 0.0},
-					{width: '10%', offset: 0.008},
-					{width: '15%', offset: 0.015},
-					{width: '20%', offset: 0.025},
-					{width: '40%', offset: 0.05},
-					{width: '50%', offset: 0.065},
-					{width: '60%', offset: 0.08},
-					{width: '65%', offset: 0.09},
-					{width: '70%', offset: 0.125},
-					{width: '75%', offset: 0.15},
-					{width: '76%', offset: 0.20},
-					{width: '76.5%', offset: 0.25},
-					{width: '77%', offset: 0.35},
-					{width: '77.5%', offset: 0.50},
-					{width: '78%', offset: 0.70},
-					{width: '79%', offset: 0.80},
-					{width: '79.5%', offset: 0.90},
-					{width: '80%', offset: 1.00}
-				], {
-					duration: 50000,
-				});
-
-				const animation = jsAnimation;
-				jsAnimation.onfinish = function() {
-					if (jsAnimation===animation) {
-						jsAnimation = undefined;
-						$container.style.width = '80%';
-					}
-				}
-			}, 0);
-		},
-		response() {},
-		end() {
-			if (!jsAnimation || jsAnimation.playState==='running') {
-				this._end();
-
-			} else if (jsAnimation.playbackRate===undefined) {
-				jsAnimation.cancel();
-				this._end();
-
-			} else {
-				jsAnimation.onfinish = this._end;
-				jsAnimation.playbackRate = 500;
-			}
-		},
-		abort() {
-			$container.style.visibility = 'hidden';
-		},
-		destroy() {
+		useTimeout(() => {
 			jsAnimation && jsAnimation.cancel();
-		},
-		_end() {
+			jsAnimation = $container.animate([
+				{width: '2%', offset: 0.0},
+				{width: '10%', offset: 0.008},
+				{width: '15%', offset: 0.015},
+				{width: '20%', offset: 0.025},
+				{width: '40%', offset: 0.05},
+				{width: '50%', offset: 0.065},
+				{width: '60%', offset: 0.08},
+				{width: '65%', offset: 0.09},
+				{width: '70%', offset: 0.125},
+				{width: '75%', offset: 0.15},
+				{width: '76%', offset: 0.20},
+				{width: '76.5%', offset: 0.25},
+				{width: '77%', offset: 0.35},
+				{width: '77.5%', offset: 0.50},
+				{width: '78%', offset: 0.70},
+				{width: '79%', offset: 0.80},
+				{width: '79.5%', offset: 0.90},
+				{width: '80%', offset: 1.00}
+			], {
+				duration: 50000,
+			});
+
+			const animation = jsAnimation;
+			jsAnimation.onfinish = function() {
+				if (jsAnimation===animation) {
+					jsAnimation = undefined;
+					$container.style.width = '80%';
+				}
+			}
+		}, 0);
+
+		function endAnimation() {
 			jsAnimation && jsAnimation.pause();
 
 			const width = $container.getBoundingClientRect().width;
@@ -149,6 +122,30 @@ function createJSProgress($container)
 					$container.style.visibility = 'hidden';
 				}
 			}
+		}
+
+		return {
+			response() {},
+			replace() {},
+			end() {
+				if (!jsAnimation || jsAnimation.playState==='running') {
+					endAnimation();
+
+				} else if (jsAnimation.playbackRate===undefined) {
+					jsAnimation.cancel();
+					endAnimation();
+
+				} else {
+					jsAnimation.onfinish = endAnimation;
+					jsAnimation.playbackRate = 500;
+				}
+			},
+			abort() {
+				$container.style.visibility = 'hidden';
+			},
+			destroy() {
+				jsAnimation && jsAnimation.cancel();
+			},
 		}
 	}
 }
--- hooks/progress.js
+++ hooks/progress.js
@@ -1,16 +1,36 @@
 
-import { dom } from 'appgine/closure'
+import { dom, uri } from 'appgine/closure'
 import { isSwapping } from '../src/update'
+import { getElementTarget } from '../src/lib/target'
 
-import { withModuleContext, useContext } from 'appgine/hooks'
+import { withModuleContext, useContext, bindContext } from 'appgine/hooks'
 import { useListen } from 'appgine/hooks/channel'
 import { useEvent } from 'appgine/hooks/event'
-import { bindApi } from 'appgine/hooks/callback'
 
 
-export function useProgress(ident, api) {
-	return useContext(() => createListener(ident, api));
+export function useProgress(accept, api) {
+	return internalUseProgress({}, accept, api);
 }
+
+export function useAppProgress(accept, api) {
+	return internalUseProgress({ ajax: false }, accept, api);
+}
+
+export function useAjaxProgress(accept, api) {
+	return internalUseProgress({ ajax: true }, accept, api);
+}
+
+export function useFormProgress(accept, api) {
+	return internalUseProgress({ form: true }, accept, api);
+}
+
+
+function internalUseProgress(defaultAccept, accept, api) {
+	const paramApi = api || accept;
+	const paramAccept = (api && typeof accept==='function') ? { accept } : (accept instanceof HTMLElement ? { $element: accept } : (api && accept || {}));
+	return useContext(() => createListener({ ...paramAccept, ...defaultAccept }, paramApi));
+}
+
 
 const $link = document.createElement('a');
 
@@ -23,16 +43,51 @@ const autoSubmitElement = [];
 
 withModuleContext(module, function() {
 	useEvent(document, 'focusin', function(e) {
-		const found = findListeners(e.target);
+		for (let foundListener of findListeners(e.target)) {
+			for (let swapListener of swapListeners) {
+				if (foundListener.form===false || foundListener.form!==swapListener.form) {
+					continue;
 
-		for (let i=0; i<found.length; i++) {
-			found[i].state.runned = false;
+				} else if (foundListener.formName===null || foundListener.formName!==swapListener.formName) {
+					continue;
 
-			if (swapListeners.length>i) {
-				found[i].state.runned = swapListeners[i].state.runned;
+				} else if (foundListener.autoSubmitRequest || foundListener.appRequest || Object.keys(foundListener.ajaxRequestList).length>0) {
+					continue;
+				}
 
-				if (swapListeners[i].state.appnum>0) {
-					found[i].api.tail && found[i].api.tail();
+				let swapRequest = null;
+				for (let requestnum of Object.keys(swapListener.ajaxRequestList)) {
+					const ajaxRequest = swapListener.ajaxRequestList[requestnum];
+
+					if (ajaxRequest.request.formName===foundListener.formName) {
+						swapRequest = {...ajaxRequest.request};
+						break;
+					}
+				}
+
+				if (swapListener.appRequest && swapListener.appRequest.request.formName===foundListener.formName) {
+					swapRequest = {...swapListener.appRequest.request};
+				}
+
+				if (swapRequest===null) {
+					continue;
+				}
+
+				swapRequest.$element = e.target;
+				swapRequest.endpoint = uri.createForm(foundListener.$form, e.target);
+
+				const autoSubmitRequest = foundListener.createApi(null, true, {...swapRequest});
+
+				if (autoSubmitRequest) {
+					foundListener.autoSubmitRequest = autoSubmitRequest;
+					foundListener.autoSubmitRequest.abortTimeout = setTimeout(function() {
+						foundListener.autoSubmitRequest = null;
+
+						if (autoSubmitRequest.api.abort) {
+							autoSubmitRequest.api.abort();
+						}
+					}, 300);
+					break;
 				}
 			}
 		}
@@ -43,9 +98,7 @@ withModuleContext(module, function() {
 	useEvent(document, 'focusout', function(e) {
 		if (isSwapping()) {
 			swapListeners = findListeners(e.target);
-			setTimeout(function() {
-				swapListeners = [];
-			}, 200);
+			setTimeout(() => { swapListeners = []; }, 200);
 		}
 	});
 
@@ -56,12 +109,22 @@ withModuleContext(module, function() {
 		}
 
 		for (let listener of findListeners($element)) {
-			if (listener.$element && listener.state.runned && listener.state.appnum===-1) {
-				listener.api.prestart && listener.api.prestart();
+			if (listener.autoSubmitRequest) {
+				clearTimeout(listener.autoSubmitRequest.abortTimeout);
+
+			} else if (listener.$element && listener.form) {
+				const formTarget = getElementTarget($element) || getElementTarget($form)
+				const formName = $form.getAttribute('data-ajax') || $form.getAttribute('name') || null;
+
+				const isAjax = formTarget==='_ajax';
+				const isGlobal = false;
+				const endpoint = uri.createForm($form, $element);
+				const request = { isAjax, isGlobal, endpoint, $element, formName, labels: {}, level: 0 };
+
+				listener.autoSubmitRequest = listener.createApi(null, true, {...request})
 			}
 		}
 	});
-
 
 	useListen('auto-submit', 'submit', function($form, $element) {
 		if (autoSubmitForm.indexOf($form)!==-1) {
@@ -73,84 +136,111 @@ withModuleContext(module, function() {
 	useListen('auto-submit', 'destroy', abortAutoSubmit);
 
 	useListen('app.request', 'start', (endpoint, { $element, requestnum }) => onRequestStart(false, true, endpoint, $element, requestnum));
-	useListen('app.request', 'response', ({ requestnum }) => onRequestResponse(requestnum));
-	useListen('app.request', 'end', ({ requestnum }) => onRequestEnd(requestnum));
-	useListen('app.request', 'abort', ({ requestnum }) => onRequestAbort(requestnum));
+	useListen('app.request', 'response', ({ requestnum }) => onRequestActionInternal(requestnum, 'response'));
+	useListen('app.request', 'upload', ({ requestnum, loaded, total }) => onRequestActionInternal(requestnum, 'progress', loaded, total));
+	useListen('app.request', 'error', ({ requestnum, errno, error }) => onRequestActionEnd(requestnum, 'error', errno, error));
+	useListen('app.request', 'abort', ({ requestnum }) => onRequestActionEnd(requestnum, 'abort'));
+	useListen('app.request', 'end', ({ requestnum }) => setTimeout(() => onRequestActionEnd(requestnum, 'end'), 100));
 
-	useListen('ajax.request', 'start', function(endpoint, { $element, requestnum, isGlobal }) {
-		onRequestStart(true, isGlobal, endpoint, $element, requestnum);
-	});
-
-	useListen('ajax.request', 'response', ({ requestnum }) => onRequestResponse(requestnum));
-	useListen('ajax.request', 'end', ({ requestnum }) => onRequestEnd(requestnum));
-	useListen('ajax.request', 'abort', ({ requestnum }) => onRequestAbort(requestnum));
+	useListen('ajax.request', 'start', (endpoint, { $element, requestnum, isGlobal }) => onRequestStart(true, isGlobal, endpoint, $element, requestnum));
+	useListen('ajax.request', 'response', ({ requestnum }) => onRequestActionInternal(requestnum, 'response'));
+	useListen('ajax.request', 'abort', ({ requestnum }) => onRequestActionEnd(requestnum, 'abort'));
+	useListen('ajax.request', 'end', ({ requestnum }) => setTimeout(() => onRequestActionEnd(requestnum, 'end'), 100));
 });
 
 
-export function createListener(ident, api)
+export function createListener(acceptObj, createApi)
 {
 	const listener = function() {
 		if (listeners.indexOf(listener)!==-1) {
 			listeners.splice(listeners.indexOf(listener), 1);
 		}
+
+		tryRequestActionCall(null, listener.autoSubmitRequest, 'abort', true);
+		tryRequestActionCall(null, listener.appRequest, null, true);
+
+		for (let requestnum of Object.keys(listener.ajaxRequestList)) {
+			tryRequestActionCall(null, listener.ajaxRequestList[requestnum], null, true)
+		}
+	}
+
+	if (!createApi) {
+		return listener;
 	}
 
 	listeners.push(listener);
 
-	listener.api = bindApi(api || ident);
-	listener.$element = null;
-	listener.endpoint = [];
+	let currentListenerApi = null;
+	const tmpCreateApi = bindContext(function(isAutoSubmit, request) {
+		currentListenerApi = listener.form ? createApi(isAutoSubmit, request) : createApi(request);
+		return currentListenerApi && currentListenerApi.dispose;
+	});
+
+	listener.createApi = (requestnum, isAutoSubmit, request) => {
+		tmpCreateApi(isAutoSubmit, request);
+		return currentListenerApi===false ? null : { request, requestnum, api: currentListenerApi||{} };
+	};
+
+	listener.endpointList = [];
 	listener.labels = [];
-	listener.state = {
-		appnum: -1,
-		ajaxnum: [],
-		runned: false,
+	listener.form = false;
+	listener.formName = null;
+	listener.ajax = acceptObj.ajax !== undefined ? acceptObj.ajax : null;
+
+	listener.appRequest = null;
+	listener.autoSubmitRequest = null;
+	listener.ajaxRequestList = {};
+
+	if (typeof acceptObj.accept === 'function') {
+		listener.accept = acceptObj.accept;
 	}
 
-	if (listener.api && ident) {
-		if (ident.$element) {
-			listener.$element = ident.$element;
+	if (acceptObj.$element) {
+		listener.$element = acceptObj.$element;
+
+		if (acceptObj.form) {
+			const $form = dom.getAncestor(acceptObj.$element, 'form');
+			const formName = $form && ($form.getAttribute('data-ajax') || $form.getAttribute('name')) || null;
+			listener.formName = formName;
+			listener.form = true;
+			listener.$form = $form;
 		}
 
-		if (ident.labels) {
-			listener.labels = Array.isArray(ident.labels) ? ident.labels : [ident.labels];
-		}
+	} else if (acceptObj.form) {
+		listener.form = true;
+	}
 
-		if (ident.label) {
-			listener.labels.push(ident.label);
-		}
+	if (acceptObj.labels) {
+		listener.labels = Array.isArray(acceptObj.labels) ? acceptObj.labels : [acceptObj.labels];
 
-		if (ident.endpoint) {
-			listener.endpoint = Array.isArray(ident.endpoint) ? ident.endpoint : [ident.endpoint];
-			listener.endpoint = listener.endpoint.map(endpoint => {
-				$link.href = endpoint;
-				$link.href = $link.href;
-				return $link.href;
-			});
-		}
+	} else if (acceptObj.label) {
+		listener.labels.push(acceptObj.label);
+	}
+
+	if (acceptObj.endpoint) {
+		listener.endpointList = Array.isArray(acceptObj.endpoint) ? acceptObj.endpoint : [acceptObj.endpoint];
+		listener.endpointList = listener.endpointList.map(endpoint => {
+			$link.href = endpoint;
+			$link.href = $link.href;
+			return $link.href;
+		});
 	}
 
 	for (let requestnum of Object.keys(appRequestList)) {
 		requestnum = parseInt(requestnum, 10);
 		const request = appRequestList[requestnum]
 
-		const matchFn = listener => listener.labels.some(label => request.labels[label]);
-		const [found, foundLevel] = matchListners([listener], request.$element, request.endpoint, request.allowGlobal, matchFn);
+		const foundListeners = matchListners([listener], request.$element, request, true);
 
-		if (found.length>0 && (request.level===0 || foundLevel>=request.level)) {
-			request.level = foundLevel;
-			for (let label of listener.labels) {
-				request.labels[label] = true;
-			}
-
-			listener.api.start && listener.api.start();
-			listener.state.runned = true;
+		if (foundListeners.length>0) {
+			listener.labels.forEach(label => request.labels[label] = true);
 
 			if (request.isAjax) {
-				listener.state.ajaxnum.push(requestnum);
+				const ajaxRequest = listener.createApi(requestnum, false, {...request});
+				ajaxRequest && (listener.ajaxRequestList[requestnum] = ajaxRequest);
 
 			} else {
-				listener.state.appnum = requestnum;
+				listener.appRequest = listener.createApi(requestnum, false, {...request});
 			}
 		}
 	}
@@ -159,32 +249,63 @@ export function createListener(ident, api)
 }
 
 
-function matchListners(listeners, $element, endpoint, allowGlobal, matchFn)
+function matchListners(listeners, $element, request=null, matchLabels=false)
 {
 	let selector;
-	if (endpoint) {
-		$link.href = endpoint;
+	let endpoint;
+
+	if (request && request.endpoint) {
+		$link.href = request.endpoint;
 		$link.href = $link.href;
 
 		endpoint = $link.href;
 		selector = 'a[href="'+endpoint.replace(/["\\]/g, '\\$&')+'"]';
 	}
 
-	let foundLevel = 0;
 	let found = [];
+	let foundLevel = request && request.level || 0;
 	for (let listener of listeners) {
-		let matched = matchFn ? matchFn(listener) : false;
-		if (endpoint && listener.endpoint.indexOf(endpoint)!==-1) {
+		let matched = true;
+
+		if (listener.ajax && request && request.isAjax===false) {
+			continue;
+		}
+
+		if (listener.ajax===false && request && request.isAjax) {
+			continue;
+		}
+
+		if (matchLabels) {
+			matched = listener.labels.length===0 || listener.labels.some(label => request.labels[label]);
+		}
+
+		if (listener.accept) {
+			if (!matched || !request) {
+				continue;
+
+			} else if (!listener.accept({...request})) {
+				continue;
+			}
+		}
+
+		if (endpoint && listener.endpointList.indexOf(endpoint)!==-1) {
 			matched = true;
 
 		} else if (selector && listener.$element && listener.$element.querySelector(selector)) {
 			matched = true;
 
 		} else if (listener.$element) {
-			matched = matched || !!($element && dom.contains(listener.$element, $element));
+			matched = matched && !!($element && (dom.contains(listener.$element, $element) || dom.contains($element, listener.$element)));
 
-		} else if (allowGlobal) {
-			matched = matched || listener.endpoint.length===0;
+		} else if (request && request.isGlobal) {
+			matched = matched || listener.endpointList.length===0;
+		}
+
+		if (request && listener.formName) {
+			matched = matched && listener.formName===request.formName;
+
+		} else if (listener.form && request && !request.formName) {
+			matched = false;
 		}
 
 		if (matched) {
@@ -207,12 +328,16 @@ function matchListners(listeners, $element, endpoint, allowGlobal, matchFn)
 		}
 	}
 
-	return [found, foundLevel];
+	if (request) {
+		request.level = foundLevel;
+	}
+
+	return found;
 }
 
 
 function findListeners($element) {
-	return matchListners(listeners, $element)[0];
+	return matchListners(listeners, $element);
 }
 
 
@@ -221,9 +346,8 @@ function abortAutoSubmit($form) {
 
 	if (index!==-1) {
 		for (let listener of findListeners(autoSubmitElement[index])) {
-			if (listener.$element && listener.state.runned && listener.state.appnum===-1) {
-				listener.api.prestart && listener.api.abort && listener.api.abort();
-			}
+			tryRequestActionCall(null, listener.autoSubmitRequest, 'abort', true);
+			listener.autoSubmitRequest = null;
 		}
 
 		autoSubmitForm.splice(index, 1);
@@ -237,87 +361,98 @@ function onRequestStart(isAjax, isGlobal, endpoint, $element, requestnum) {
 		return false;
 	}
 
-	for (let request of Object.values(appRequestList)) {
-		if (isAjax===false && request.isAjax===false && request.requestnum<requestnum) {
-			delete appRequestList[appRequestList.indexOf(request)];
-		}
-	}
-
 	if (autoSubmitForm.indexOf($element)!==-1) {
 		$element = autoSubmitElement[autoSubmitForm.indexOf($element)];
 	}
 
-	const [found, foundLevel] = matchListners(listeners, $element, isAjax || $element.tagName==='A' ? endpoint : null, isGlobal);
+	const $form = $element.tagName==='FORM' ? $element : $element.form;
+	const formName = $form && ($form.getAttribute('data-ajax') || $form.getAttribute('name')) || null;
 
-	appRequestList[requestnum] = { isAjax, isGlobal, endpoint, $element, labels: {}, level: foundLevel };
+	appRequestList[requestnum] = { isAjax, isGlobal, endpoint, $element, formName, labels: {}, level: 0 }
+
+	const foundListeners = matchListners(listeners, $element, appRequestList[requestnum], false);
 
 	for (let listener of listeners) {
-		if (found.indexOf(listener)!==-1) {
-			if (requestnum>listener.state.appnum) {
-				for (let label of listener.labels) {
-					appRequestList[requestnum].labels[label] = true;
-				}
+		if (foundListeners.indexOf(listener)!==-1) {
+			listener.labels.forEach(label => appRequestList[requestnum].labels[label] = true);
 
-				if (listener.state.appnum>-1 || listener.state.ajaxnum.length>0) {
-					listener.api.restart && listener.api.restart();
+			const autoSubmitRequest = $form && listener.autoSubmitRequest || null;
 
-				} else {
-					listener.api.start && listener.api.start();
-				}
-
-				listener.state.runned = true;
-
-				if (isAjax) {
-					listener.state.ajaxnum.push(requestnum);
-
-				} else {
-					listener.state.appnum = requestnum;
-				}
+			if (autoSubmitRequest) {
+				autoSubmitRequest.requestnum = requestnum;
+				listener.autoSubmitRequest = null;
 			}
 
-		} else if (isAjax===false && listener.state.appnum>0 && listener.state.appnum<requestnum) {
-			listener.state.appnum = requestnum;
+			if (isAjax) {
+				const ajaxRequest = autoSubmitRequest || listener.createApi(requestnum, false, {...appRequestList[requestnum]});
+				ajaxRequest && (listener.ajaxRequestList[requestnum] = ajaxRequest);
+
+			} else if (autoSubmitRequest===null && listener.appRequest && listener.appRequest.api.replace) {
+				listener.appRequest.requestnum = requestnum;
+				tryRequestActionCall(null, listener.appRequest, 'replace', false, {...appRequestList[requestnum]})
+
+			} else {
+				tryRequestActionCall(null, listener.appRequest, 'abort', true);
+				listener.appRequest = autoSubmitRequest || listener.createApi(requestnum, false, {...appRequestList[requestnum]});
+			}
+
+		} else if (listener.appRequest && isAjax===false) {
+			listener.appRequest.requestnum = requestnum;
 		}
 	}
 }
 
 
-function onRequestResponse(requestnum) {
+function onRequestActionEnd(requestnum, action, ...args) {
+
+	delete appRequestList[requestnum];
+
 	for (let listener of listeners) {
-		if (listener.state.appnum===requestnum) {
-			listener.api.response && listener.api.response();
+		tryRequestActionCall(requestnum, listener.appRequest, action, true, ...args);
+		tryRequestActionCall(requestnum, listener.ajaxRequestList[requestnum], action, true, ...args);
+
+		if (listener.appRequest && listener.appRequest.requestnum===requestnum) {
+			listener.appRequest = null;
 		}
+
+		if (Object.keys(listener.ajaxRequestList).length===1) {
+			tryRequestActionCall(requestnum, listener.ajaxRequestList[requestnum], 'finish', action==='end');
+		}
+
+		delete listener.ajaxRequestList[requestnum];
 	}
 }
 
 
-function onRequestEnd(requestnum) {
-	setTimeout(() => onRequestEndOrAbort(requestnum, 'end'), 100);
+function onRequestActionInternal(requestnum, action, ...args) {
+	for (let listener of listeners) {
+		tryRequestActionCall(requestnum, listener.appRequest, action, false, ...args);
+		tryRequestActionCall(requestnum, listener.ajaxRequestList[requestnum], action, false, ...args);
+	}
 }
 
 
-function onRequestAbort(requestnum) {
-	onRequestEndOrAbort(requestnum, 'abort');
-}
+function tryRequestActionCall(requestnum, request, action, dispose, ...args) {
+	if (!request || (requestnum!==null && request.requestnum!==requestnum)) {
+		return;
+	}
 
-function onRequestEndOrAbort(requestnum, type) {
-	if (appRequestList[requestnum]) {
-		delete appRequestList[requestnum];
+	if (action===null && ['abort', 'error', 'end'].indexOf(request.action)===-1) {
+		action = request.action==='response' ? 'end' : 'abort';
+	}
 
-		for (let listener of listeners) {
-			let ended = false;
-			if (listener.state.appnum===requestnum) {
-				listener.state.appnum = -1;
-				ended = true;
+	try {
+		if (action) {
+			request.action = action;
 
-			} else if (listener.state.ajaxnum.indexOf(requestnum)!==-1) {
-				listener.state.ajaxnum.splice(listener.state.ajaxnum.indexOf(requestnum), 1);
-				ended = true;
-			}
-
-			if (ended && listener.state.appnum===-1 && listener.state.ajaxnum.length===0) {
-				listener.api[type] && listener.api[type]();
+			if (request.api[action]) {
+				request.api[action](...args);
 			}
 		}
+
+	} catch (e) {}
+
+	if (dispose && request.api.dispose) {
+		request.api.dispose(...args);
 	}
 }
